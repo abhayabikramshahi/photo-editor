7057697f845c7c521d9a12a91a3e24fdab95e386
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-09-10 14:14:37 +0545

    chnaged in router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import React, { useState, useRef } from "react";
-import Toolbar from "./components/Toolbar";
-import Editor from "./components/Editor";
-import Filters from "./components/Filters";
-import './App.css'
+import React, { useRef } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import './App.css';
+
 function App() {
-  const [image, setImage] = useState(null);
-  const [filters, setFilters] = useState({
-    brightness: 100,
-    grayscale: 0,
-    sepia: 0,
-  });
   const imgRef = useRef(null);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
-      <h1 className="text-3xl font-bold mb-6 text-red-600">Abhaya Photo Editor</h1>
-      <Toolbar setImage={setImage} imgRef={imgRef} />
-      {image && <Editor image={image} filters={filters} imgRef={imgRef} />}
-      {image && <Filters filters={filters} setFilters={setFilters} />}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home imgRef={imgRef} />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useRef } from "react";
+import Toolbar from "../components/Toolbar";
+import Editor from "../components/Editor";
+import Filters from "../components/Filters";
+function App() {
+  const [image, setImage] = useState(null);
+  const [filters, setFilters] = useState({
+    brightness: 100,
+    grayscale: 0,
+    sepia: 0,
+  });
+  const imgRef = useRef(null);
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
+      <h1 className="text-3xl font-bold mb-6 text-red-600">Abhaya Photo Editor</h1>
+      <Toolbar setImage={setImage} imgRef={imgRef} />
+      {image && <Editor image={image} filters={filters} imgRef={imgRef} />}
+      {image && <Filters filters={filters} setFilters={setFilters} />}
+    </div>
+  );
+}
+
+export default App;
